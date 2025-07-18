ded19e93e36370ce03a86c37fd0faabe022c1d1e
Andrii <andripolishchuk@gmail.com>
2025-07-19 00:46:27 +0300

    (feat): add api gateway redirect implementation

---
+++ user-service-latest/src/main/java/video/platform/user_service/latest/controller/UserController.java
@@ -0,0 +1,16 @@
+package video.platform.user_service.latest.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping
+    public ResponseEntity<String> ping() {
+        return ResponseEntity.ok("User service is working!");
+    }
+}
